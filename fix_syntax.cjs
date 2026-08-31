e3e9727bb31ee07f131ae7647f6a6ef01ff12a11
weronedatabase-tech <werone.database@gmail.com>
2026-09-01 07:39:35 +0800

    Environment Promotion from Overseas-Trip-Planner-Dev

--- fix_syntax.cjs
+++ fix_syntax.cjs
@@ -1,17 +1,10 @@
 const fs = require('fs');
+let code = fs.readFileSync('frontend/js/logistics.js', 'utf8');
 
-function fixSyntax(file) {
-    let content = fs.readFileSync(file, 'utf8');
-    content = content.replace(/return \\`<div/g, 'return `<div');
-    content = content.replace(/<\/div>\\`;/g, '</div>`;');
-    
-    // Also, inside that string, we need to make sure ${fRoleColor} is not escaped inside a template literal, unless it's supposed to be literally `${fRoleColor}` which it shouldn't. Wait, we want it evaluated!
-    // In my patch I did \\\${fRoleColor} which became \${fRoleColor} in the file.
-    // If it's inside `return \`...\``, it should be `${fRoleColor}`!
-    content = content.replace(/\\\$\{/g, '${');
-    
-    fs.writeFileSync(file, content, 'utf8');
-}
+code = code.replace(
+    /data-fullname="\$\{fullName\}">\`\s*<div class="flex justify-between items-start w-full gap-2">/g,
+    `data-fullname="\${fullName}">
+    <div class="flex justify-between items-start w-full gap-2">`
+);
 
-fixSyntax('frontend/js/main.js');
-fixSyntax('frontend/js/profile.js');
+fs.writeFileSync('frontend/js/logistics.js', code);
